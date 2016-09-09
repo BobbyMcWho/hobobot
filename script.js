2e12fa74b1602cf73c4c9521fd12999addd009b3
BobbyMcWho <BobbyMcWho@gmail.com>
2016-09-09 00:31:50 -0400

    fix?

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var Discord = require("discord.js");
 var Key = require('./token.json');
-
+var Sesame = Key.token;
 var bot = new Discord.Client();
 
 bot.on("message", function(message)
@@ -11,4 +11,4 @@ bot.on("message", function(message)
     }
 });
 
-bot.login(Key[0]);
+bot.login(Sesame);
