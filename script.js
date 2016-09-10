3d73925db63ca4b3c3717406e99f92d364d8c894
BobbyMcWho <BobbyMcWho@gmail.com>
2016-09-10 01:45:32 -0400

    Revert "a"
    This reverts commit 42f5b74cfc4648e76da86558774d2ddbd8ab1be6.

--- script.js
+++ script.js
@@ -10,38 +10,8 @@ client.on('message', message => {
     }
 });
 
-<<<<<<< HEAD
 client.on('ready', () => {
   console.log('I am ready!');
-=======
-function hobobaby(){
-  var now = new Date();
-  var babyDue = new Date(2016, 11, 10);
-  var currentTime = now.getTime();
-  var babyBorn = babyDue.getTime();
-  var timeRemain = babyBorn - currentTime;
-  var s = Math.floor(timeRemain/1000);
-  var m = Math.floor(s/60);
-  var h = Math.floor(m/60);
-  var d = Math.floor(h/24);
-
-  h %= 24;
-  m %= 60;
-  s %= 60;
-
-  h = (h < 10) ? "0" + h : h;
-  m = (m < 10) ? "0" + m : m;
-  s = (s < 10) ? "0" + s : s;
-
-  var timePhrase = d + " days, " + h + " hours, " + m + " minutes, " + s + " seconds remaining until hobobaby is born.";
-}
-bot.on("message", function(message)
-       {
-    if(message.content === "!hobobaby")
-    {
-    message.reply(timePhrase);
-    }
->>>>>>> parent of ac08aab... fixes
 });
 
 client.login(Sesame);
