2a643e33683f649988554f21d51bff5632a2efc4
BobbyMcWho <BobbyMcWho@gmail.com>
2016-09-10 19:56:26 -0400

    byebye butterfree

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ client.on('message', message => {
       message.channel.sendFile('./clefable.gif');
     }
     else if (message.content.toLowerCase().startsWith(prefix + "commands")) {
-      message.channel.sendMessage('```'+prefix+'whoishobo```,```'+prefix+'hoblet```,```'+prefix+'int```,```'+prefix+'zzz```,```'+prefix+'clefable```');
+      message.channel.sendMessage('```'+prefix+'whoishobo``````'+prefix+'hoblet``````'+prefix+'int``````'+prefix+'zzz``````'+prefix+'clefable```');
     }
 
 
