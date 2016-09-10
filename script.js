ab499f9f0ac20f9ab26b994aa9316b21ec4877b6
BobbyMcWho <BobbyMcWho@gmail.com>
2016-09-09 23:54:58 -0400

    features

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
-var Discord = require("discord.js");
-var Key = require('./token.json');
-var Sesame = Key.token;
-var bot = new Discord.Client();
-
-bot.on("message", function(message)
+var Discord = require("discord.js"),
+    Key = require('./token.json'),
+    Sesame = Key.token,
+    bot = new Discord.Client(),
+    deadline = '2015-11-10';
+bot.on("message", message =>
        {
     if(message.content === "Who is hobo?")
     {
     message.reply("I am Hobo!");
+    }
+});
+
+function hobobaby(){
+  var now = new Date();
+  var babyDue = new Date(2016, 11, 10);
+  var currentTime = now.getTime();
+  var babyBorn = babyDue.getTime();
+  var timeRemain = babyBorn - currentTime;
+  var s = Math.floor(timeRemain/1000);
+  var m = Math.floor(s/60);
+  var h = Math.floor(m/60);
+  var d = Math.floor(h/24);
+
+  h %= 24;
+  m %= 60;
+  s %= 60;
+
+  h = (h < 10) ? "0" + h : h;
+  m = (m < 10) ? "0" + m : m;
+  s = (s < 10) ? "0" + s : s;
+
+  var timePhrase = d + " days, " + h + " hours, " + m + " minutes, " + s + " seconds remaining until hobobaby is born.";
+}
+bot.on("message", message =>
+       {
+    if(message.content === "!hobobaby")
+    {
+    message.reply(timePhrase);
     }
 });
 
