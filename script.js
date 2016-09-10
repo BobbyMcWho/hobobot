1e768c066d6a8a4d6223889e7c8a4305495eb729
BobbyMcWho <BobbyMcWho@gmail.com>
2016-09-10 02:03:11 -0400

    FIXES

--- script.js
+++ script.js
@@ -1,17 +1,45 @@
-var Discord = require('discord.js');
-var client = new Discord.Client();
-var Key = require('./token.json');
-var    Sesame = Key.token;
+/*jshint esversion: 6 */
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const Key = require('./token.json');
+const Sesame = Key.token;
+
+function hobobaby(){
+  var now = new Date();
+  var babyDue = new Date(2016, 11, 10);
+  var currentTime = now.getTime();
+  var babyBorn = babyDue.getTime();
+  var timeRemain = babyBorn - currentTime;
+  var s = Math.floor(timeRemain/1000);
+  var m = Math.floor(s/60);
+  var h = Math.floor(m/60);
+  var d = Math.floor(h/24);
+
+  h %= 24;
+  m %= 60;
+  s %= 60;
+
+  h = (h < 10) ? "0" + h : h;
+  m = (m < 10) ? "0" + m : m;
+  s = (s < 10) ? "0" + s : s;
+
+  var timePhrase = d.toString() + " days, " + h.toString() + " hours, " + m.toString() + " minutes, " + s.toString() + " seconds remaining until hobobaby is born.";
+}
+
 
 client.on('message', message => {
-    if(message.content === 'Who is he?')
+  let prefix = '~';
+  if(!msg.content.startsWith(prefix)) return;
+  if(msg.author.bot) return;
+    if (message.content.startsWith(prefix + 'foo'))
     {
-    message.reply('I am He!');
+    message.channel.sendMessage('bar');
     }
-});
-
-client.on('ready', () => {
-  console.log('I am ready!');
+    else if (message.content.startsWith(prefix + 'hobobaby')) {
+      hobobaby();
+      message.channel.sendMessage(timePhrase);
+    }
 });
 
 client.login(Sesame);
