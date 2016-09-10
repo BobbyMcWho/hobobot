c3f41b9d9ac9ed411fc8bf1105c71195be3053bc
BobbyMcWho <BobbyMcWho@gmail.com>
2016-09-10 02:06:19 -0400

    asdf

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function hobobaby(){
 
 
 client.on('message', message => {
-  let prefix = '~';
+  let prefix = '!';
   if(!msg.content.startsWith(prefix)) return;
   if(msg.author.bot) return;
     if (message.content.startsWith(prefix + 'foo'))
