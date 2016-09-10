26861ac83deba9adf5a1d94358f65a3e9825f1d4
BobbyMcWho <BobbyMcWho@gmail.com>
2016-09-10 00:29:38 -0400

    FIXES

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
-const Discord = require("discord.js");
-const cLient = newDiscord.Client();
+const Discord = require('discord.js');
+const client = new Discord.Client();
 const Key = require('./token.json');
 var    Sesame = Key.token;
-var    deadline = '2015-11-10';
 
-function hobobaby(){
-  var now = new Date();
-  var babyDue = new Date(2016, 11, 10);
-  var currentTime = now.getTime();
-  var babyBorn = babyDue.getTime();
-  var timeRemain = babyBorn - currentTime;
-  var s = Math.floor(timeRemain/1000);
-  var m = Math.floor(s/60);
-  var h = Math.floor(m/60);
-  var d = Math.floor(h/24);
-
-  h %= 24;
-  m %= 60;
-  s %= 60;
-
-  h = (h < 10) ? "0" + h : h;
-  m = (m < 10) ? "0" + m : m;
-  s = (s < 10) ? "0" + s : s;
-
-  var timePhrase = d + " days, " + h + " hours, " + m + " minutes, " + s + " seconds remaining until hobobaby is born.";
-}
+// function hobobaby(){
+//   var now = new Date();
+//   var babyDue = new Date(2016, 11, 10);
+//   var currentTime = now.getTime();
+//   var babyBorn = babyDue.getTime();
+//   var timeRemain = babyBorn - currentTime;
+//   var s = Math.floor(timeRemain/1000);
+//   var m = Math.floor(s/60);
+//   var h = Math.floor(m/60);
+//   var d = Math.floor(h/24);
+//
+//   h %= 24;
+//   m %= 60;
+//   s %= 60;
+//
+//   h = (h < 10) ? "0" + h : h;
+//   m = (m < 10) ? "0" + m : m;
+//   s = (s < 10) ? "0" + s : s;
+//
+//   var timePhrase = d.toString() + " days, " + h.toString() + " hours, " + m.toString() + " minutes, " + s.toString() + " seconds remaining until hobobaby is born.";
+// }
 
 client.on("message", function(message)
        {
@@ -34,12 +33,12 @@ client.on("message", function(message)
     }
 });
 
-client.on("message", function(message)
-       {
-    if(message.content === "!hobobaby")
-    {hobobaby();
-    message.reply(timePhrase);
-    }
-});
+// client.on("message", function(message)
+//        {
+//     if(message.content === "!hobobaby")
+//     {hobobaby();
+//     message.reply(timePhrase);
+//     }
+// });
 
 client.login(Sesame);
