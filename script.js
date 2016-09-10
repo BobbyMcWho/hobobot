456722b4c979749aec413ffa3bd9b858f6c3982a
BobbyMcWho <BobbyMcWho@gmail.com>
2016-09-10 19:52:56 -0400

    return!

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ client.on('message', message => {
       message.channel.sendFile('./clefable.gif');
     }
     else if (message.content.toLowerCase().startsWith(prefix + "commands")) {
-      message.channel.sendMessage('```'+prefix+'whoishobo``` \n ```'+prefix+'hoblet``` \n ```'+prefix+'int``` \n ```'+prefix+'zzz``` \n ```'+prefix+'clefable```');
+      message.channel.sendMessage('```'+prefix+'whoishobo``` \r ```'+prefix+'hoblet``` \r ```'+prefix+'int``` \r ```'+prefix+'zzz``` \r ```'+prefix+'clefable```');
     }
 
 
