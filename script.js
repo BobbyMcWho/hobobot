28f5b150640707df5e73cf5a6b67e3f71f95fd24
Robert McDonald <BobbyMcWho@users.noreply.github.com>
2016-09-12 14:52:57 -0400

    user

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ client.on('message', message => {
     else if (message.content.toLowerCase().startsWith(prefix + "abyss")) {
       message.channel.sendMessage("I don't know what I'm doing, I'm confused."); 
     }
-    else if (message.content.toLowerCase().startsWith('hi ' + ClientUser.id)) {
+    else if (message.content.toLowerCase().startsWith('hi ' + ClientUser.username)) {
       message.channel.sendMessage('hi'+ message.author.username);
     }
 
