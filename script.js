e8d356ca16ca5cae98427df2d42c89cb2c1e5d51
Robert McDonald <BobbyMcWho@users.noreply.github.com>
2016-09-27 14:46:33 -0400

    yt temp

--- script.js
+++ script.js
@@ -281,7 +281,7 @@ else if (message.content.toLowerCase().startsWith(prefix + "wiki")) {
       let title = ytResponse.items[0].snippet.title;
       let description = ytResponse.items[0].snippet.description;
       let channel = ytResponse.items[0].snippet.channel;
-      message.channel.sendFile(`https://www.youtube.com/watch?v=${videoId}`,'',`You may find this video from **${channel}** interesting:`);
+      message.channel.sendMessage(`You may find this video from **${channel}** interesting: \n https://www.youtube.com/watch?v=${videoId}`);
     }
   });
 }
