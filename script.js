b8ee6d71dd5de092a1d6df7472890c041b286c74
Robert McDonald <BobbyMcWho@users.noreply.github.com>
2016-10-12 15:06:17 -0400

    double rainbow

--- script.js
+++ script.js
@@ -249,8 +249,10 @@ else if ((message.content.startsWith(prefix + "purge")) && ((message.author.id =
     let messagecount = parseInt(params[0]);
     message.channel.fetchMessages({limit: messagecount})
         .then(messages =>{
-             let filteredMessages = messages.filter(m => {m.content.startsWith(prefix) || (m.author.id === client.user.id)});
+             let filteredMessages = messages.filter(m => {m.content.startsWith(prefix)})
+              let filteredMessages2 = messages.filter(m => {m.author.id === client.user.id});
               message.channel.bulkDelete(filteredMessages);
+      message.channel.bulkDelete(filteredMessages2);
 })}
 else if ((message.content.startsWith(prefix + "log")) && ((message.author.id === '186693404288090114'))) {
     client.guilds.find('id','187346688497680385').channels.find('id','229058004866039808').sendMessage("logged");
