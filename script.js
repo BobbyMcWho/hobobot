79aeff6efe332556b63a1a81355f5032b3b4e5af
BobbyMcWho <BobbyMcWho@gmail.com>
2016-10-16 20:29:10 -0400

    delete test

--- script.js
+++ script.js
@@ -318,7 +318,8 @@ else if (message.content.toLowerCase().startsWith(prefix + 'delete')
 //  && (message.member.hasPermission('MANAGE_MESSAGES'))
 ) {
   let messageId = params[0];
-  message.channel.fetchMessage(messageId).delete();
+  message.channel.fetchMessage(messageId)
+    .then(msg => msg.delete());
    }
 
 //else if (message.content.toLowerCase().startsWith(prefix + "teams")) {
