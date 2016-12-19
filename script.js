b4570c3713f1ec3fc102be89a30982294ac65758
Robert McDonald <BobbyMcWho@users.noreply.github.com>
2016-12-19 15:33:48 -0500

    Try url without object literals

--- script.js
+++ script.js
@@ -415,10 +415,10 @@ else if (message.content.toLowerCase().startsWith(prefix + "unban")) {
     
 }
   else if (message.content.toLowerCase().startsWith(prefix + "time")) {
+  let timeZone = params[0];
+  let url = "http://www.timeapi.org/"+timeZone+"/now.json?format=\a%20\b%20\d%20\I:\M\p%20";
 
-  let url = `http://www.timeapi.org/${params[0]}/now.json?format=\\a%20\\b%20\\d%20\\I:\\M\\p%20`;
-
-  request(url, {timeout: 5000},(error,response,body) => {
+  request(url, (error,response,body) => {
     if (error) {
       return console.error('upload failed:', error);
     }
