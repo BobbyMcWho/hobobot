f4aa7dbb9ee33b422a5e643693deedaad50c11e6
BobbyMcWho <BobbyMcWho@gmail.com>
2017-01-05 00:31:28 -0500

    retesting

--- script.js
+++ script.js
@@ -218,31 +218,31 @@ else if (message.content.toLowerCase().startsWith(prefix + "urbantest")) {
       let example = urbanResponse.list[0].example;
       let word = urbanResponse.list[0].word;
       //message.channel.sendMessage(`**${word}:**\n${definition} \n\uD83D\uDC4D ${thumbsup} \uD83D\uDC4E ${thumbsdown} \n \nExample: ${example}`);
-      // const embed = new Discord.RichEmbed()
-      //   .setTitle(word)
-      //   .setAuthor('Urban Dictionary', './ud.jpg')
-      //   .setColor('#FDCE0C')
-      //   .setDescription('definition')
-      //   .setFooter('Urban Dictionary', './ud.jpg')
-      //   .setImage('https://goo.gl/D3uKk2')
-      //   .setThumbnail('https://goo.gl/lhc6ke')
-      //   /*
-      //   * Takes a Date object, defaults to current date.
-      //   */
-      //   .setTimestamp()
-      //   .setURL('https://discord.js.org/#/docs/main/indev/class/RichEmbed')
-      //   .addField('Field Title', 'Field Value')
-      //   /*
-      //   * Inline fields may not display as inline if the thumbnail and/or image is too big.
-      //   */
-      //   .addField('Inline Field', 'Hmm 🤔', true)
-      //   /*
-      //   * Blank field, useful to create some space.
-      //   */
-      //   .addField('\u200b', '\u200b', true)
-      //   .addField('Second (3rd place) Inline Field', 'I\'m in the ZOONE', true);
+      const embed = new Discord.RichEmbed()
+        .setTitle(word)
+        .setAuthor('Urban Dictionary', './ud.jpg')
+        .setColor('#FDCE0C')
+        .setDescription('definition')
+        .setFooter('Urban Dictionary', './ud.jpg')
+        .setImage('https://goo.gl/D3uKk2')
+        .setThumbnail('https://goo.gl/lhc6ke')
+        /*
+        * Takes a Date object, defaults to current date.
+        */
+        .setTimestamp()
+        .setURL('https://discord.js.org/#/docs/main/indev/class/RichEmbed')
+        .addField('Field Title', 'Field Value')
+        /*
+        * Inline fields may not display as inline if the thumbnail and/or image is too big.
+        */
+        .addField('Inline Field', 'Hmm 🤔', true)
+        /*
+        * Blank field, useful to create some space.
+        */
+        .addField('\u200b', '\u200b', true)
+        .addField('Second (3rd place) Inline Field', 'I\'m in the ZOONE', true);
 
-      // message.channel.sendEmbed(embed);
+      message.channel.sendEmbed(embed);
         }
         }
   });
