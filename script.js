e5cae98d3f1e4edaba0cffd8e8bc1c32eebd7547
Robert McDonald <BobbyMcWho@users.noreply.github.com>
2017-01-06 11:58:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -514,7 +514,7 @@ else if (message.content.toLowerCase().startsWith(prefix + "time")) {
 }
   else if (message.content.toLowerCase().startsWith(prefix + "clever")) {
    let query = params.join[" "];
-   cBot.create(function (err, message.author.id) {
+   cBot.create(function (err, `{$message.author.id}`) {
    cBot.ask(query, function (err, response) {
       const embed = new Discord.RichEmbed()
   .setAuthor("Clever Hobo")
