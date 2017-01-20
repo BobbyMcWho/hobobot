475688447ecba5d45c7293df1687824c97e86ee6
BobbyMcWho <BobbyMcWho@gmail.com>
2017-01-19 19:37:39 -0500

    .

--- script.js
+++ script.js
@@ -769,7 +769,7 @@ client.on('message', message => {
         let change = $('#price-panel').find('.id-price-change').text().trim().split("\n")[0]
         let percentChange = $('#price-panel').find('.id-price-change').text().trim().split("\n")[1];
         let companyName = $('#appbar').find('appbar-center > .appbar-snippet-primary').text().trim();
-        console.log($('#appbar').find('appbar-center > .appbar-snippet-primary'));
+        console.log($('#appbar').find('appbar-center > .appbar-snippet-primary').text());
         let companySymbol = $('#appbar').find('appbar-center > .appbar-snippet-secondary').text().trim();
       // message.channel.sendMessage(`**${stockID}:** ${lastTrade}USD ${change} ${percentChange}`);
         let color = (parseFloat(change) < 0) ? 13715510 : 39219;
