1882a7dc21cc4f1a376c22f4e9f0d0cc8a35a204
BobbyMcWho <BobbyMcWho@gmail.com>
2017-02-13 23:38:06 -0500

    append

--- script.js
+++ script.js
@@ -845,19 +845,19 @@ client.on('message', message => {
   //****************END TEST
 
 else if (message.content.toLowerCase().startsWith(prefix + "qwerty")) {
-   let response;
+   let response = "";
    let newMember = client.guilds.get('164791530614161408').members.find('nickname','Abyss');
   if (
     //(!oldMember.presence.game.streaming) && 
     (newMember.presence.game && newMember.presence.game.streaming)) {
       let temp = newMember.presence.game.url.split('/');
       let userName = temp[temp.length-1];
-      let url = `https://api.twitch.tv/kraken/streams/${userName}?client_id=${twitchClient}?callback=foo`;
+      let url = `https://api.twitch.tv/kraken/streams/${userName}?client_id=${twitchClient}`;
       console.log(url);
         request(url, (error, response, body) => {
           if (!error && response.statusCode === 200) { 
             console.log(body);          
-            response = JSON.parse(body);
+            response += JSON.parse(body);
           }
         }); //end of request function
 console.log(response);
